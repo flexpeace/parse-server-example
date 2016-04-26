7af3d16ed966f648ff13b18dc202d97ddaa77cba
Ernest Appiah <flexpeace@gmail.com>
2016-04-26 11:39:26 +0300

    updated index.js

--- index.js
+++ index.js
@@ -11,12 +11,24 @@ if (!databaseUri) {
   console.log('DATABASE_URI not specified, falling back to localhost.');
 }
 
-var api = new ParseServer({
+/**var api = new ParseServer({
   databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID || 'myAppId',
   masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
+  liveQuery: {
+    classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
+  }
+});
+*/
+
+var api = new ParseServer({
+  databaseURI: databaseUri || 'mongodb://heroku_j58x643k:q5b8vv9t4k7l2ggc9jkmh5r7k3@ds021771.mlab.com:21771/heroku_j58x643k',
+  cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
+  appId: process.env.APP_ID || 'PNi24UFGGDbrJnIVNf3a5pCMLAkRf4kQW4K2AI7x',
+  masterKey: process.env.MASTER_KEY || 'PJUw1sEtPq6AgmJUZbxGB8ZnbpHxyQaJYbtuoT3j', //Add your master key here. Keep it secret!
+  serverURL: process.env.SERVER_URL || 'https://sidraice.herokuapp.com/parse',  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
   }
