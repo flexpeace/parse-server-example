bd15a26243725ea5b009f3d1d6fbae9fcd4f9944
Ernest Appiah <flexpeace@gmail.com>
2016-04-26 11:49:56 +0300

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ if (!databaseUri) {
 */
 
 var api = new ParseServer({
-  databaseURI: databaseUri || 'mongodb://heroku_j58x643k:q5b8vv9t4k7l2ggc9jkmh5r7k3@ds021771.mlab.com:21771/heroku_j58x643k',
+  databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID || 'PNi24UFGGDbrJnIVNf3a5pCMLAkRf4kQW4K2AI7x',
   masterKey: process.env.MASTER_KEY || 'PJUw1sEtPq6AgmJUZbxGB8ZnbpHxyQaJYbtuoT3j', //Add your master key here. Keep it secret!
